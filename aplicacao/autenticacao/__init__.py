84e1a929a24859f582f5ce978f8541d39f1f74e3
JulianoSaroba123 <julianosaroba19.jsp@gmail.com>
2025-09-09 09:24:56 -0300

    Padroniza blueprint de autenticação e corrige rotas

--- aplicacao/autenticacao/__init__.py
+++ aplicacao/autenticacao/__init__.py
@@ -1,15 +1,9 @@
-bp_autenticacao = Blueprint('autenticacao', __name__, template_folder='templates')
 from flask import Blueprint
 
-bp_autenticacao = Blueprint('autenticacao', __name__, template_folder='templates')
+bp_autenticacao = Blueprint(
+    'autenticacao',
+    __name__,
+    template_folder='templates'
+)
 
 from . import rotas
-
-from flask import Blueprint
-
-bp_autenticacao = Blueprint('autenticacao', __name__, template_folder='templates')
-
-from . import rotas
-from aplicacao import create_app
-import os
-app = create_app()
--- aplicacao/autenticacao/rotas.py
+++ aplicacao/autenticacao/rotas.py
@@ -1,7 +1,8 @@
 
-from flask import render_template, request, redirect, url_for, flash, session
+
+from flask import render_template
 from . import bp_autenticacao
 
-@bp_autenticacao.route('/login', methods=['GET', 'POST'])
+@bp_autenticacao.route('/login')
 def login():
     return render_template('autenticacao/login.html')
