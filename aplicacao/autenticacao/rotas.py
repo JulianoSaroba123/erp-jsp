fca516f47ad82f5a464227222407c2d63b68eadf
JulianoSaroba123 <julianosaroba19.jsp@gmail.com>
2025-09-09 09:38:21 -0300

    Adiciona rota inicial / para renderizar login

--- aplicacao/autenticacao/rotas.py
+++ aplicacao/autenticacao/rotas.py
@@ -6,3 +6,7 @@ from . import bp_autenticacao
 @bp_autenticacao.route('/login')
 def login():
     return render_template('autenticacao/login.html')
+
+@bp_autenticacao.route('/')
+def index():
+    return render_template('autenticacao/login.html')
