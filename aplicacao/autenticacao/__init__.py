a9ba72ccd119fb294846b5997e1cff43d3777bf9
JulianoSaroba123 <julianosaroba19.jsp@gmail.com>
2025-09-09 09:12:16 -0300

    Corrige import do Blueprint

--- aplicacao/autenticacao/__init__.py
+++ aplicacao/autenticacao/__init__.py
@@ -1,12 +1,9 @@
 bp_autenticacao = Blueprint('autenticacao', __name__, template_folder='templates')
-
 from flask import Blueprint
 
 bp_autenticacao = Blueprint('autenticacao', __name__, template_folder='templates')
 
 from . import rotas
-from .autenticacao import bp_autenticacao
-app.register_blueprint(bp_autenticacao)
 
 from flask import Blueprint
 
